6b583804fc202982c70edfa1627b28b59e418862
RumRage <claravillarruel4@gmail.com>
2025-09-09 23:36:52 -0300

    ADD profile page

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -61,14 +61,14 @@ export const useAuthStore = create((set, get) => ({
     }
   },
 
-  updatedProfile: async (data) => {
+  updateProfile: async (data) => {
     set({ isUpdatingProfile: true });
     try {
       const res = await axiosInstance.put("/auth/update-profile", data);
       set({ authUser: res.data });
       toast.success("Profile updated successfully");
     } catch (error) {
-      console.log("Error in updateProfile:", error);
+      console.log("error in update profile:", error);
       toast.error(error.response.data.message);
     } finally {
       set({ isUpdatingProfile: false });
